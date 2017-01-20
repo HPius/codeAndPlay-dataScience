44333435e944eff0a8157058f150a0570de9d77e
Hübl, Pius <Pius.Huebl@senacor.com>
2017-01-20 15:46:16 +0100

    Ready for CodeAndPlay 2017

--- src/main/java/com/senacor/InitialerCheck.java
+++ src/main/java/com/senacor/InitialerCheck.java
@@ -3,7 +3,8 @@ package com.senacor;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
-import org.apache.spark.sql.DataFrame;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
 import org.apache.spark.sql.RowFactory;
 import org.apache.spark.sql.SQLContext;
 import org.apache.spark.sql.types.DataTypes;
@@ -30,7 +31,7 @@ public class InitialerCheck {
         SQLContext sqlContext = new SQLContext(jsc);
 
 
-        DataFrame dataFrame = sqlContext.createDataFrame(
+        Dataset<Row> dataFrame = sqlContext.createDataFrame(
                 initRDD.map(s -> RowFactory.create(s)),
                 DataTypes.createStructType(
                         new StructField[]{DataTypes.createStructField("Strings", DataTypes.StringType, false)}));
